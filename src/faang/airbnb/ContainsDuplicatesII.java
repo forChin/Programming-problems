20972b062db9ecf1a7c61eb58e40207217a7fd26
unknown <chingiz409@gmail.com>
2020-03-10 15:55:59 +0600

    new solution - ContainsDuplicatesII

---
+++ src/faang/airbnb/ContainsDuplicatesII.java
@@ -0,0 +1,30 @@
+package faang.airbnb;
+
+import java.util.HashMap;
+
+public class ContainsDuplicatesII {
+
+	public static void main(String[] args) {
+		ContainsDuplicatesII c = new ContainsDuplicatesII();
+		
+		System.out.println(c.containsNearbyDuplicate(new int[] {1, 2, 1, 3, 4}, 2)); // answer is true
+		System.out.println(c.containsNearbyDuplicate(new int[] {1, 2, 3, 4, 1}, 2)); // answer is false
+		System.out.println(c.containsNearbyDuplicate(new int[] {1, 2, 3, 4, 5, 6}, 5)); // answer is false
+	}
+
+	public boolean containsNearbyDuplicate(int[] nums, int k) {
+		HashMap<Integer, Integer> map = new HashMap<>();
+		
+		for(int i = 0; i < nums.length; i++) {
+			int current = nums[i];
+			
+			if(map.containsKey(current) && i - map.get(current) <= k ) {
+				return true;
+			} else {
+				map.put(current, i);
+			}
+		}
+		
+		return false;
+	}
+}
