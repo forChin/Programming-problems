e8920dc6a95ef2770f6856ea51ab0d8182860c24
unknown <chingiz409@gmail.com>
2020-03-10 15:57:27 +0600

    add new comments

--- src/faang/airbnb/ContainsDuplicatesII.java
+++ src/faang/airbnb/ContainsDuplicatesII.java
@@ -2,6 +2,12 @@ package faang.airbnb;
 
 import java.util.HashMap;
 
+/* Given an array of integers and an integer k, 
+ * find out whether there are two distinct indices i and 
+ * j in the array such that nums[i] = nums[j] and 
+ * the absolute difference between i and j is at most k.
+ */
+
 public class ContainsDuplicatesII {
 
 	public static void main(String[] args) {
