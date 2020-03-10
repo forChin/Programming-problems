11d364e01426fdafdf43ea14527a8b67f6ae9fe3
unknown <chingiz409@gmail.com>
2020-03-10 15:40:05 +0600

    new solution - MajorityElement

---
+++ src/faang/adobe/MajorityElement.java
@@ -0,0 +1,34 @@
+package faang.adobe;
+
+import java.util.HashMap;
+
+public class MajorityElement {
+
+	public static void main(String[] args) {
+		MajorityElement m = new MajorityElement();
+		
+		System.out.println(m.majorityElement(new int[] {1, 2, 3, 4, 5})); // answer is -1
+		System.out.println(m.majorityElement(new int[] {1, 2, 2, 3, 2})); // answer is 2
+		System.out.println(m.majorityElement(new int[] {10, 11, 1002, 10, 10, 10, 10})); // answer is 10
+	}
+	
+	public int majorityElement(int[] nums) {
+		if(nums.length == 1) {
+			return nums[0];
+		}
+		
+		HashMap<Integer, Integer> map = new HashMap<>();
+		
+		for(int i : nums) {
+			if(map.containsKey(i) && map.get(i) + 1 > nums.length / 2) {
+				return i;
+			} else if(map.containsKey(i)) {
+				map.put(i, map.get(i) + 1);
+			} else {
+				map.put(i, 1);
+			}
+		}
+		
+		return -1;
+	}
+}
