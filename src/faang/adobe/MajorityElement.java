6d3bf601a8abc98483d5a97c29544674ee84554e
unknown <chingiz409@gmail.com>
2020-03-10 15:41:29 +0600

    new comments

--- src/faang/adobe/MajorityElement.java
+++ src/faang/adobe/MajorityElement.java
@@ -2,6 +2,13 @@ package faang.adobe;
 
 import java.util.HashMap;
 
+/* Given an array of size n, find the majority element. 
+ * The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+ * 
+ * You may assume that the array is non-empty and 
+ * the majority element always exist in the array.
+ */
+
 public class MajorityElement {
 
 	public static void main(String[] args) {
